13afb432e9725acb72cc6ee081111cc2c1d04194
Iftekhar-007 <shawonsp534@gmail.com>
2025-10-05 23:11:37 +0600

    from shawon 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h2>Hello from shawon</h2>;
+  return (
+    <div>
+      <h2>Hello from shawon</h2>
+      <p>Hello again from shawon</p>
+    </div>
+  );
 }
