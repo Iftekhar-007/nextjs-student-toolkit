79155f9998a92552971f7895e488af9a68a5e0df
Md Soriful hasan <sorifullhasan300@gmail.com>
2025-10-08 20:29:15 +0600

    test commit for setup

--- src/app/components/Banner/Banner.tsx
+++ src/app/components/Banner/Banner.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 
 const Banner = () => {
-  return (
-    <div>
-      <h2>OKay</h2>
-      <p>Banner doing</p>
-      <p>another</p>
-      <p>another from soriful</p>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Banner;
